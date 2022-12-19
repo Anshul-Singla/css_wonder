19890ffa451b2e54f6c62e071911fd3fe3ad46c1
Anshul-Singla <asingla2807@gmail.com>
2022-12-20 00:14:24 +0530

    cards js done project done

--- onClickOpenCard/script.js
+++ onClickOpenCard/script.js
@@ -1,1 +1,16 @@
-const boxes
+const boxes = document.querySelectorAll(".box");
+console.log('boxes:', boxes)
+
+boxes.forEach(box => 
+   
+    box.addEventListener('click' , () => {
+        removeClass();
+        box.classList.add('desiredBoxDesign')
+    } )
+)
+
+function  removeClass ( ){
+    boxes.forEach(box => 
+        box.classList.remove("desiredBoxDesign")
+    )
+}
